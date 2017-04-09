ec8200c578afceb09099b79087feb6c66323ee1b
Proxying <kieran_quigley_CFC@hotmail.com>
2017-04-09 19:53:06 +0100

    Update to 1.0.4

--- src/main/java/uk/co/proxying/tabmanager/PluginInfo.java
+++ src/main/java/uk/co/proxying/tabmanager/PluginInfo.java
@@ -10,6 +10,6 @@ public class PluginInfo {
 
 	public static final String ID = "tabmanager";
 	public static final String NAME = "TabManager";
-	public static final String VERSION = "1.0.3";
+	public static final String VERSION = "1.0.4";
 	public static final String DESCRIPTION = "Customize your tab list in Sponge.";
 }
